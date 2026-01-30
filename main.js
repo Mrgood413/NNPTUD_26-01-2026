823e8a85138bfc3846581774950e2010174617db
Mrgood413 <oanhdoanthi@gmail.com>
2026-01-30 09:06:50 +0700

    them tang id

--- main.js
+++ main.js
@@ -14,41 +14,52 @@ async function GetData() {
     }
 }
 async function Save() {
-    let id = document.getElementById("id_txt").value;
+    let id = document.getElementById("id_txt").value.trim();
     let title = document.getElementById("title_txt").value;
     let views = document.getElementById("views_txt").value;
 
-    let getItem = await fetch('http://localhost:3000/posts/' + id);
-    if (getItem.ok) {
-        let res = await fetch('http://localhost:3000/posts/'+id, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title: title,
-                views: views
-            })
-        })
-        
-    } else {
-        //fetch -> HTTP POST
-        let res = await fetch('http://localhost:3000/posts', {
+    // TRƯỜNG HỢP TẠO MỚI (ID TRỐNG)
+    if (id === "") {
+        let res = await fetch('http://localhost:3000/posts');
+        let posts = await res.json();
+
+        let maxId = 0;
+        for (let p of posts) {
+            let numId = parseInt(p.id);
+            if (numId > maxId) maxId = numId;
+        }
+
+        let newId = (maxId + 1).toString();
+
+        await fetch('http://localhost:3000/posts', {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                id: newId,
+                title: title,
+                views: views,
+                isDeleted: false
+            })
+        });
+    } 
+    // TRƯỜNG HỢP UPDATE
+    else {
+        await fetch('http://localhost:3000/posts/' + id, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 id: id,
                 title: title,
-                views: views
+                views: views,
+                isDeleted: false
             })
-        })
+        });
     }
+
     GetData();
     return false;
-
 }
+
 
 function convertObjToHTML(post) {
     return `<tr>
